03347e3702f4bf902d22f9ed5543c27c68146753
jauhar-imtikhan <bangjoe.muns@gmail.com>
2025-07-08 04:19:53 +0700

    deploy v2

---
+++ public/build/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://absensi.pandawatechnomedia.com/build/sw.js', { scope: 'https://absensi.pandawatechnomedia.com/build/' })})}
