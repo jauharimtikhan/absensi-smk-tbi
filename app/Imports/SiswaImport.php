ec1982f0d29435cb77f867141e829d0ee02473ac
jauhar-imtikhan <bangjoe.muns@gmail.com>
2025-07-11 23:52:59 +0700

    update SiswaImport class for handling duplicate NIS

--- app/Imports/SiswaImport.php
+++ app/Imports/SiswaImport.php
@@ -10,13 +10,14 @@ class SiswaImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        return new Siswa([
-            'nis'           => $row['nis'],
-            'nama_lengkap'    => $row['nama_lengkap'],
-            'status'       => $row['status'],
-            'jenis_kelamin'   => $row['jenis_kelamin'] === "null" ? null : $row['jenis_kelamin'],
-            'kelas'         => $row['kelas'],
-            'jurusan'        => $row['jurusan']
+        return  Siswa::updateOrCreate([
+            'nis' => $row['nis'],
+        ], [
+            'nama_lengkap' => $row['nama_lengkap'],
+            'status' => $row['status'],
+            'jenis_kelamin' => $row['jenis_kelamin'] === "null" ? null : $row['jenis_kelamin'],
+            'kelas' => $row['kelas'],
+            'jurusan' => $row['jurusan']
         ]);
     }
 }
